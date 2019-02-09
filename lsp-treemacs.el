3ead8eb368c714432788a0ab04e374f0c6b1f859
Alexander Miller <alexanderm@web.de>
2019-02-10 00:40:56 +0100

    Cleanup for flycheck.

--- lsp-treemacs.el
+++ lsp-treemacs.el
@@ -1,5 +1,33 @@
-(require 'treemacs-extensions)
+;; lsp-treemacs.el --- treemacs-based extensions for lsp-mode -*- lexical-binding: t -*-
+
+;; Package-Requires: ((emacs "25.2") (dash "2.11.0") (s "1.10.0") (f "0.11.0") (ht "2.2"))
+;; Homepage: https://github.com/Alexander-Miller/treemacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'lsp)
 (require 'treemacs)
+(require 'cl-lib)
+
+(defgroup lsp-treemacs nil
+  "lsp-treemacs configuration options."
+  :group 'lsp-treemacs
+  :prefix "lsp-treemacs-")
 
 (defface lsp-treemacs-project-root-error
   '((t :inherit font-lock-keyword-face :underline (:style wave :color "Red1")))
@@ -40,21 +68,29 @@
   '((1 . lsp-treemacs-project-root-error)
     (2 .  lsp-treemacs-project-root-warn)
     (3 .  lsp-treemacs-project-root-info))
-  "Alist diagnostics to face.")
+  "Alist diagnostics to face."
+  :type 'alist
+  :group 'lsp-treemacs)
 
 (defcustom lsp-treemacs-icons-map
   `((1 . ,lsp-treemacs-error)
     (2 . ,lsp-treemacs-warning)
     (3 . ,lsp-treemacs-info))
-  "Alist diagnostics to face.")
+  "Alist diagnostics to face."
+  :type 'alist
+  :group 'lsp-treemacs)
 
 (defcustom lsp-treemacs-file-face-map
   '((1 . lsp-treemacs-file-error)
     (2 . lsp-treemacs-file-warn)
     (3 . lsp-treemacs-file-info))
-  "Alist diagnostics to face.")
+  "Alist diagnostics to face."
+  :type 'alist
+  :group 'lsp-treemacs)
 
 (defun lsp-treemacs--root-folders ()
+  "Get a list of lsp root folders.
+Will return an alist mapping display names to absolute paths."
   (let ((diagnostics (lsp-diagnostics)))
     (->> (lsp-session)
          lsp-session-folders
@@ -80,10 +116,12 @@
           (call-interactively #'lsp-execute-code-action))))))
 
 (defun lsp-treemacs--open (&rest _)
+  "Open the file at point."
   (interactive)
   (find-file (button-get (treemacs-node-at-point) :key)))
 
 (defun lsp-treemacs--open-error (&rest _)
+  "Open the error at point."
   (interactive)
   (-let [(file . diag) (button-get (treemacs-node-at-point) :key)]
     (find-file file)
@@ -92,6 +130,7 @@
     (forward-char (lsp-diagnostic-column diag))))
 
 (defun lsp-treemacs--face (root-folder diagnostics)
+  "Get face for ROOT-FOLDER based on DIAGNOSTICS."
   (--> diagnostics
        ht->alist
        (-keep (-lambda ((file-name . file-diagnostics))
@@ -103,17 +142,19 @@
               it)
        -min
        (assoc it lsp-treemacs-face-map)
-       rest))
+       cl-rest))
 
 (defun lsp-treemacs--diag-statistics (file-diagnostics)
+  "Format FILE-DIAGNOSTICS for display."
   (s-join
    ":"
    (-map (-lambda ((severity . diagnostics))
            (propertize (f-filename (number-to-string (length diagnostics)))
-                       'face (rest (assoc severity lsp-treemacs-file-face-map))))
+                       'face (cl-rest (assoc severity lsp-treemacs-file-face-map))))
          (-group-by 'lsp-diagnostic-severity file-diagnostics))))
 
 (defun lsp-treemacs--get-files (project-root)
+  "Get files under PROJECT-ROOT."
   (--> (lsp-diagnostics)
        ht->alist
        (-keep (-lambda ((file-name . file-diagnostics))
@@ -138,6 +179,7 @@
 (defvar treemacs-icon-error "")
 
 (defun lsp-treemacs--errors (file-name)
+  "Get diagnostics for FILE-NAME."
   (--map (cons file-name it) (gethash file-name (lsp-diagnostics))))
 
 (treemacs-define-expandable-node lsp-error
@@ -149,7 +191,7 @@
   (treemacs-render-node
    :icon (treemacs-as-icon ". " 'face 'font-lock-string-face)
    :label-form (propertize (lsp-diagnostic-message item) 'face 'default)
-   :state treemacs-lsp-files-closed-state
+   :state (with-no-warnings treemacs-lsp-files-closed-state)
    :key-form item))
 
 (treemacs-define-expandable-node lsp-files
@@ -159,37 +201,37 @@
   :ret-action 'lsp-treemacs--open
   :render-action
   (treemacs-render-node
-   :icon (case (lsp-diagnostic-severity (rest item))
+   :icon (cl-case (lsp-diagnostic-severity (cl-rest item))
            (1 treemacs-icon-error)
            (2 treemacs-icon-warning)
            (t treemacs-icon-info))
-   :label-form (propertize (lsp-diagnostic-message (rest item)) 'face 'default)
+   :label-form (propertize (lsp-diagnostic-message (cl-rest item)) 'face 'default)
    :state treemacs-lsp-error-open-state
    :key-form item))
 
 (treemacs-define-expandable-node lsp-projects
-  :icon-open treemacs-icon-root
-  :icon-closed treemacs-icon-root
+  :icon-open treemacs-icon-open
+  :icon-closed treemacs-icon-closed
   :query-function (lsp-treemacs--get-files (treemacs-button-get btn :key))
   :ret-action 'lsp-treemacs--open
   :render-action
   (treemacs-render-node
-   :icon (treemacs-icon-for-file (first item))
-   :label-form (rest item)
+   :icon (treemacs-icon-for-file (cl-first item))
+   :label-form (cl-rest item)
    :state treemacs-lsp-files-closed-state
-   :key-form (first item)))
+   :key-form (cl-first item)))
 
 (treemacs-define-expandable-node lsp-error-list
-  :icon-open treemacs-icon-root
-  :icon-closed treemacs-icon-root
+  :icon-open (treemacs-as-icon "⚠ ")
+  :icon-closed (treemacs-as-icon "⚠ ")
   :query-function (lsp-treemacs--root-folders)
   :render-action
   (treemacs-render-node
-   :icon treemacs-icon-root
-   :label-form (first item)
+   :icon treemacs-icon-lsp-projects-closed
+   :label-form (cl-first item)
    :state treemacs-lsp-projects-closed-state
    ;; :face 'default
-   :key-form (rest item))
+   :key-form (cl-rest item))
   :top-level-marker t
   :root-label "Errors"
   :root-face 'font-lock-type-face
@@ -205,13 +247,13 @@
   (treemacs-expand-lsp-error-list))
 
 (defun lsp-treemacs--after-diagnostics ()
+  "Update the diagnostics view."
   (with-demoted-errors "%s"
     (with-current-buffer (get-buffer-create "*Showcase Buffer List*")
-      (save-excursion
-        (goto-char (point-min))
-        (let ((btn (treemacs-node-at-point)))
-          (when (treemacs-is-node-expanded? btn)
-            (funcall (alist-get (treemacs-button-get btn :state) treemacs-TAB-actions-config))
-            (funcall (alist-get (treemacs-button-get btn :state) treemacs-TAB-actions-config))))))))
+      (treemacs-update-node '(:custom Buffers)))))
 
 (add-hook 'lsp-after-diagnostics-hook 'lsp-treemacs--after-diagnostics)
+
+(provide 'lsp-treemacs)
+
+;;; lsp-treemacs.el ends here
